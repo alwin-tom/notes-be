80cf2d5fc969c1e8fcd0220fc6b079c9d371c7e3
Alwin Tom <hialwin94@gmail.com>
2026-07-27 12:00:46 +0200

    Create task.service.js

---
+++ services/task.service.js
@@ -0,0 +1,110 @@
+const { getTasksClient } = require("../googleClient");
+
+
+async function getTaskLists(){
+
+    const tasks = getTasksClient();
+
+    const response =
+        await tasks.tasklists.list();
+
+    return response.data.items || [];
+}
+
+
+
+async function getTasks(taskListId){
+
+    const tasks = getTasksClient();
+
+    const response =
+        await tasks.tasks.list({
+            tasklist: taskListId
+        });
+
+    return response.data.items || [];
+}
+
+
+
+async function createTask(taskListId,data){
+
+    const tasks = getTasksClient();
+
+    const response =
+        await tasks.tasks.insert({
+
+            tasklist: taskListId,
+
+            requestBody:{
+                title:data.title,
+                notes:data.notes,
+                due:data.due
+            }
+
+        });
+
+
+    return response.data;
+}
+
+
+
+async function updateTask(
+    taskListId,
+    taskId,
+    data
+){
+
+    const tasks = getTasksClient();
+
+
+    const response =
+        await tasks.tasks.patch({
+
+            tasklist:taskListId,
+
+            task:taskId,
+
+            requestBody:data
+
+        });
+
+
+    return response.data;
+}
+
+
+
+async function deleteTask(
+    taskListId,
+    taskId
+){
+
+    const tasks=getTasksClient();
+
+
+    await tasks.tasks.delete({
+
+        tasklist:taskListId,
+
+        task:taskId
+
+    });
+
+
+    return {
+        success:true
+    };
+
+}
+
+
+
+module.exports={
+    getTaskLists,
+    getTasks,
+    createTask,
+    updateTask,
+    deleteTask
+};
